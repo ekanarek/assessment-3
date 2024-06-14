489b551a10ab4e55bc637d4885aa063ded4325a5
Emily Kanarek <emilyakanarek@gmail.com>
2024-06-14 13:24:32 -0400

    Set up random fossil generator

--- src/random-fossil.js
+++ src/random-fossil.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+import axios from 'axios';
+
+const getRandomFossil = async () => {
+    const response = await axios.get('/random-fossil.json');
+    const fossil = response.data;
+
+    const fossilImageDiv = document.getElementById('random-fossil-image');
+    const fossilNameParagraph = document.getElementById('random-fossil-name');
+
+    fossilImageDiv.innerHTML = `<img src="${fossil.img}">`;
+    fossilNameParagraph.innerText = fossil.name;
+}
+
+const randomFossilButton = document.getElementById('get-random-fossil');
+
+randomFossilButton.addEventListener('click', getRandomFossil)
